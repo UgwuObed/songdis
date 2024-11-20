82fdc13e76220d39cf76d8e2f9c928614d446eb1
UgwuObed <vjazzyvictor@gmail.com>
2024-11-20 12:36:12 +0100

    feat

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -5,7 +5,4 @@ echo "DB_HOST: $DB_HOST"
 echo "DB_PORT: $DB_PORT"
 echo "DB_DATABASE: $DB_DATABASE"
 echo "DB_USERNAME: $DB_USERNAME"
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
 php artisan migrate --force
